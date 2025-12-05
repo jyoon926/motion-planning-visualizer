f7aab65954ef09c290e4d63d043197f052a4c052
lklec <lek7518@rit.edu>
2025-12-05 15:01:33 -0500

    lint fixes

--- src/algorithms/visibilityGraph.ts
+++ src/algorithms/visibilityGraph.ts
@@ -3,7 +3,6 @@ import type { AlgorithmOutput, AlgorithmPhase, AlgorithmStep, Point } from '../u
 import { runAStar } from './astar';
 import { BinarySearchTree, BinarySearchTreeNode } from './bst';
 
-
 interface NodeEdge {
   dist: number;
   edge: [Point, Point];
@@ -96,7 +95,7 @@ export function computeVisibilityGraph(
     endStepIndex: steps.length - 1,
   });
 */
-// NAIVE IMPLEMENTATION
+  // NAIVE IMPLEMENTATION
   const ccw = (A: Point, B: Point, C: Point): boolean => (C.y - A.y) * (B.x - A.x) > (B.y - A.y) * (C.x - A.x);
   const segmentsIntersect = (A: Point, B: Point, C: Point, D: Point): boolean =>
     ccw(A, C, D) !== ccw(B, C, D) && ccw(A, B, C) !== ccw(A, B, D);
@@ -190,7 +189,6 @@ function SegmentsIntersect(A, B, C, D):
     endStepIndex: steps.length - 1,
   });
 
-
   // --- PHASE 3: PATHFINDING (A*) ---
   const phase3Id = 'astar';
   const startIdx3 = steps.length;
@@ -226,7 +224,6 @@ While OpenSet not empty:
 
   return { steps, phases };
 }
-   
 
 /**
  * Find all visible edges from the current point in O(n log n) time
@@ -235,33 +232,35 @@ While OpenSet not empty:
  * @param polygons all polygons
  * @returns the visible edges
  */
-function findVisibleEdges(currP: number, vertices: Point[], polygons: Point[][]): [Point, Point][]{
+function findVisibleEdges(currP: number, vertices: Point[], polygons: Point[][]): [Point, Point][] {
   const currPoint: Point = vertices[currP];
-  const visibleEdges : [Point, Point][] = [];
-  
+  const visibleEdges: [Point, Point][] = [];
+
   const distComparator = (a: NodeEdge, b: NodeEdge): number => {
     if (a.dist < b.dist) return -1;
     if (a.dist > b.dist) return 1;
     return 0;
-  }
-  
+  };
+
   const bst = new BinarySearchTree<NodeEdge>(distComparator);
 
   const calculateIntersection = (edge1: [Point, Point], edge2: Point[]): Point => {
-    return {x: 0, y: 0}; //TODO
-  }
+    return { x: 0, y: 0 }; //TODO
+  };
 
-  const calculateDistance = (p: Point): number =>{
-    return Math.sqrt(Math.pow(vertices[currP].x - p.x,2) + Math.pow(vertices[currP].y - p.y,2));
-  }
+  const calculateDistance = (p: Point): number => {
+    return Math.sqrt(Math.pow(vertices[currP].x - p.x, 2) + Math.pow(vertices[currP].y - p.y, 2));
+  };
 
   const inOrderIntersection = (
-    bst: BinarySearchTree<NodeEdge>, node: BinarySearchTreeNode<NodeEdge> | undefined, 
-    wiMinus1: Point, wi: Point
+    bst: BinarySearchTree<NodeEdge>,
+    node: BinarySearchTreeNode<NodeEdge> | undefined,
+    wiMinus1: Point,
+    wi: Point
   ): boolean => {
     if (node) {
       inOrderIntersection(bst, node.leftNode, wiMinus1, wi);
-      if (segmentsIntersect(node.data.edge[0], node.data.edge[1], wiMinus1, wi)){
+      if (segmentsIntersect(node.data.edge[0], node.data.edge[1], wiMinus1, wi)) {
         return true;
       }
       inOrderIntersection(bst, node.rightNode, wiMinus1, wi);
@@ -269,45 +268,47 @@ function findVisibleEdges(currP: number, vertices: Point[], polygons: Point[][])
       return false;
     }
     return false;
-  }
+  };
 
   // DEBUGGING Note: isVisible is the main problem (works when replaced with naive)
   const isVisible = (p: Point, w: number): boolean => {
     // if intersects interior of obstacle of which wi is vertex then return false
     const incidents = incidentEdges.get(getKeyString(vertices[w]));
-    if (incidents !== undefined && incidents.length > 1){
+    if (incidents !== undefined && incidents.length > 1) {
       const wprev = incidents[0];
       const wnext = incidents[1];
-      if (orientation(wprev, vertices[w], p) > 0 &&
-          orientation(vertices[w], wnext, p) > 0){
+      if (orientation(wprev, vertices[w], p) > 0 && orientation(vertices[w], wnext, p) > 0) {
         return false;
       }
     }
-    
-    console.log("isVisible: w=",w);
+
+    console.log('isVisible: w=', w);
     // if wi = 1 or wi-1 is not on the segment pwi
-    if (w === 1 || orientation(p, vertices[w], vertices[mod(w-1, vertices.length)]) !== 0){
+    if (w === 1 || orientation(p, vertices[w], vertices[mod(w - 1, vertices.length)]) !== 0) {
       // then search bst for the edge in the leftmost leaf
-      let left = bst.leftmost(bst.root)?.data
-      if (left == undefined){ 
-        console.log("isVisible: undefined leftmost node", bst);
-        return false; 
-      }
-      if (left.edge == undefined){console.log("isVisible: leftmost edge undefined", left)}
-      // if e exists and pwi instersects e return false
-      if (segmentsIntersect(left.edge[0], left.edge[1], p, vertices[w])){
+      const left = bst.leftmost(bst.root)?.data;
+      if (left == undefined) {
+        console.log('isVisible: undefined leftmost node', bst);
         return false;
-      } else { // else return true
+      }
+      if (left.edge == undefined) {
+        console.log('isVisible: leftmost edge undefined', left);
+      }
+      // if e exists and pwi instersects e return false
+      if (segmentsIntersect(left.edge[0], left.edge[1], p, vertices[w])) {
+        return false;
+      } else {
+        // else return true
         return true;
       }
     } else {
       // else if wi-1 is not visible return false
-      if (!isVisible(p, mod(w-1, vertices.length))){
+      if (!isVisible(p, mod(w - 1, vertices.length))) {
         return false;
-      // else search bst for an edge that intersects wi-1wi
+        // else search bst for an edge that intersects wi-1wi
       } else {
         // search bst for edge that intersects w-1,w
-        if (inOrderIntersection(bst, bst.root, vertices[mod(w-1, vertices.length)], vertices[w])){
+        if (inOrderIntersection(bst, bst.root, vertices[mod(w - 1, vertices.length)], vertices[w])) {
           // if edge exists, return false
           return false;
         } else {
@@ -317,9 +318,9 @@ function findVisibleEdges(currP: number, vertices: Point[], polygons: Point[][])
       }
     }
   };
-  
- //DEBUGGING Note: everything works with this naive version
- /*
+
+  //DEBUGGING Note: everything works with this naive version
+  /*
   const isVisible = (p1: Point, p2: Point): boolean => {
     for (const polygon of polygons) {
       let selfIntersectionCounter = 0;
@@ -353,51 +354,55 @@ function findVisibleEdges(currP: number, vertices: Point[], polygons: Point[][])
   // cross product:  a x b = ax*by - ay*bx
   const cross = (a: Point, b: Point): number => {
     return a.x * b.y - a.y * b.x;
-  }
+  };
 
   /**
    * Compute distance to the intersection between
    * ray: origin + dist * direction  (t ≥ 0)
    * segment: C + u * (D - C)  (0 ≤ u ≤ 1)
    */
-    const intersectionDistance = (origin: Point, direction: Point, C: Point, D: Point): number | null => {
-      const seg_dir = {x: D.x - C.x, y: D.y - C.y};
-      const rxs = cross(direction, seg_dir);
-      const qp = {x: C.x - origin.x, y: C.y - origin.y};
+  const intersectionDistance = (origin: Point, direction: Point, C: Point, D: Point): number | null => {
+    const seg_dir = { x: D.x - C.x, y: D.y - C.y };
+    const rxs = cross(direction, seg_dir);
+    const qp = { x: C.x - origin.x, y: C.y - origin.y };
 
-      //if r × s == 0, they're parallel or collinear
-      if (rxs === 0) return null;
+    //if r × s == 0, they're parallel or collinear
+    if (rxs === 0) return null;
 
-      const dist = cross(qp, seg_dir) / rxs;
+    const dist = cross(qp, seg_dir) / rxs;
 
-      //if t >= 0 we are in front of ray origin
-      if (dist < 0) return null;
+    //if t >= 0 we are in front of ray origin
+    if (dist < 0) return null;
 
-      return dist;
-    }
-
+    return dist;
+  };
 
   /**
-     * Returns the determinant of 3 points in order to find their orientation.
-     * @return negative value for negative orientation (clockwise),
-     *         positive value for positive orientation (counter-clockwise),
-     *         or zero for co-linear points
-     */
+   * Returns the determinant of 3 points in order to find their orientation.
+   * @return negative value for negative orientation (clockwise),
+   *         positive value for positive orientation (counter-clockwise),
+   *         or zero for co-linear points
+   */
   // orientation(p, w/edge.p1, edge.p2): + for clockwise (insert edge), - for ccw(delete edge)
-  const orientation = (p: Point, q: Point, r: Point): number => 
-    ((q.x * r.y) + (p.x * q.y) + (p.y * r.x)) - ((q.x * p.y) + (r.x * q.y) + (r.y * p.x));
-  
-  const rayDirection = (theta: number): Point => ({x:Math.cos(theta), y:Math.sin(theta)});
+  const orientation = (p: Point, q: Point, r: Point): number =>
+    q.x * r.y + p.x * q.y + p.y * r.x - (q.x * p.y + r.x * q.y + r.y * p.x);
 
-  const stringToEdge = (s: string) : [Point, Point] => {
-    const strs = s.split(",");
-    return [{x: parseInt(strs[0]), y: parseInt(strs[1])}, {x: parseInt(strs[2]), y: parseInt(strs[3])}];
+  const rayDirection = (theta: number): Point => ({ x: Math.cos(theta), y: Math.sin(theta) });
+
+  const stringToEdge = (s: string): [Point, Point] => {
+    const strs = s.split(',');
+    return [
+      { x: parseInt(strs[0]), y: parseInt(strs[1]) },
+      { x: parseInt(strs[2]), y: parseInt(strs[3]) },
+    ];
   };
 
   const mod = (dividend: number, divisor: number): number => {
-    if (divisor === 0) { throw new Error("Cannot divide by zero");}
+    if (divisor === 0) {
+      throw new Error('Cannot divide by zero');
+    }
     return ((dividend % divisor) + divisor) % divisor;
-  }
+  };
 
   /*
   sort all obstacle vertices according to clockwise angle that the half-line from p to each obstacles makes with the positive x-axis (use code from homework?)
@@ -411,72 +416,77 @@ function findVisibleEdges(currP: number, vertices: Point[], polygons: Point[][])
   */
 
   // case of only start and end points to make things faster
-  if (polygons.length === 0 && vertices.length === 2){
+  if (polygons.length === 0 && vertices.length === 2) {
     return [[vertices[0], vertices[1]]];
   }
 
   // build map of all edges and their incident edges
-  const allEdges : Map<string, string[]> = new Map(); // all edges by string id and incident edges
-  const incidentEdges : Map<string, Point[]> = new Map();
-  for (const p of polygons){
+  const allEdges: Map<string, string[]> = new Map(); // all edges by string id and incident edges
+  const incidentEdges: Map<string, Point[]> = new Map();
+  for (const p of polygons) {
     const l = p.length;
-    console.log("add edges from polygon: ", p);
-    for (let i = 0; i < l; i++){
-      allEdges.set(getEdgeKeyString(p[mod((i-1), l)], p[i]), [getEdgeKeyString(p[mod((i-2), l)], p[mod((i-1), l)]), getEdgeKeyString(p[i], p[mod((i+1), l)])]);
-      incidentEdges.set(getKeyString(p[i]), [p[mod((i-1), l)],p[mod((i+1), l)]]);
-    } 
+    console.log('add edges from polygon: ', p);
+    for (let i = 0; i < l; i++) {
+      allEdges.set(getEdgeKeyString(p[mod(i - 1, l)], p[i]), [
+        getEdgeKeyString(p[mod(i - 2, l)], p[mod(i - 1, l)]),
+        getEdgeKeyString(p[i], p[mod(i + 1, l)]),
+      ]);
+      incidentEdges.set(getKeyString(p[i]), [p[mod(i - 1, l)], p[mod(i + 1, l)]]);
+    }
   }
 
   // sort all obstacle vertices clockwise around p
-  const pointAngles: [number, number][] = []
-  for (let v = 0; v < vertices.length; v++){
-    if (v != currP){
-    pointAngles.push([v, Math.atan2(vertices[v].y - currPoint.y, vertices[v].x - currPoint.x)]);
+  const pointAngles: [number, number][] = [];
+  for (let v = 0; v < vertices.length; v++) {
+    if (v != currP) {
+      pointAngles.push([v, Math.atan2(vertices[v].y - currPoint.y, vertices[v].x - currPoint.x)]);
     }
   }
   pointAngles.sort((a, b) => a[1] - b[1]);
-  
+
   // find obstacle edges intersected by P and store in bst
-  const halflineOrigin = {x:0, y:currPoint.y};
+  const halflineOrigin = { x: 0, y: currPoint.y };
   const halfline = [halflineOrigin, currPoint];
-  for (const e of allEdges.keys()){
+  for (const e of allEdges.keys()) {
     const currEdge = stringToEdge(e);
-    if (currEdge == undefined) {console.log("line 390: stringToEdge function did not return an edge");}
-    if(segmentsIntersect(currEdge[0], currEdge[1], halfline[0], halfline[1])) {
+    if (currEdge == undefined) {
+      console.log('line 390: stringToEdge function did not return an edge');
+    }
+    if (segmentsIntersect(currEdge[0], currEdge[1], halfline[0], halfline[1])) {
       const dir = rayDirection(0);
       const d = intersectionDistance(halflineOrigin, dir, currEdge[0], currEdge[1]);
       // insert in the order they intersect halfline (distance from origin)
-      if (d !== null){
-        console.log("inserting orginal edges into bst", bst)
-        bst.insert({edge: currEdge, dist: d})
+      if (d !== null) {
+        console.log('inserting orginal edges into bst', bst);
+        bst.insert({ edge: currEdge, dist: d });
       }
     }
   }
 
-  for (const w of pointAngles){
-      const wi = w[0];
-      const wPoint = vertices[wi];
-      const wTheta = w[1];
-      const dist = calculateDistance(wPoint);
-      if (isVisible(currPoint, wi)){
-        visibleEdges.push([currPoint, wPoint]);
-        let incidents = incidentEdges.get(getKeyString(wPoint));
-        if (incidents !== undefined){
-          for (const p of incidents){
-            let e: [Point, Point] = [wPoint, p];
-            // orientation(p, w/edge.p1, edge.p2): + for clockwise (insert edge), - for ccw(delete edge)
-            if (orientation(currPoint, e[0], e[1]) > 0){
-              // insert edges incident to w on clockwise side of half-line
-              console.log("inserting edge into bst", bst);
-              bst.insert({dist: dist, edge: e})
-            } else {
-              // delete edges incident to w on counter-clockwise side of half-line
-              console.log("removing edge from bst", bst);
-              bst.delete(bst.root, {dist: dist, edge: e});
-            }
+  for (const w of pointAngles) {
+    const wi = w[0];
+    const wPoint = vertices[wi];
+    const wTheta = w[1];
+    const dist = calculateDistance(wPoint);
+    if (isVisible(currPoint, wi)) {
+      visibleEdges.push([currPoint, wPoint]);
+      const incidents = incidentEdges.get(getKeyString(wPoint));
+      if (incidents !== undefined) {
+        for (const p of incidents) {
+          const e: [Point, Point] = [wPoint, p];
+          // orientation(p, w/edge.p1, edge.p2): + for clockwise (insert edge), - for ccw(delete edge)
+          if (orientation(currPoint, e[0], e[1]) > 0) {
+            // insert edges incident to w on clockwise side of half-line
+            console.log('inserting edge into bst', bst);
+            bst.insert({ dist: dist, edge: e });
+          } else {
+            // delete edges incident to w on counter-clockwise side of half-line
+            console.log('removing edge from bst', bst);
+            bst.delete(bst.root, { dist: dist, edge: e });
           }
         }
       }
+    }
   }
 
   return visibleEdges;
